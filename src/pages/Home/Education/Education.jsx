2adc88c5c82fb7d66e2beeb89806765124ade6a0
Habibur Rahman <habibur88454@gmail.com>
2024-08-10 13:12:18 +0600

    education update

--- src/pages/Home/Education/Education.jsx
+++ src/pages/Home/Education/Education.jsx
@@ -6,7 +6,7 @@ const Education = () => {
         <div className="max-w-6xl mx-auto my-8">
             <h1 className="text-5xl font-bold  text-center mb-8 text-indigo-500 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-pink-500 ">Educational Qualification </h1>
 
-            <VerticalTimeline>
+            <VerticalTimeline lineColor='pink'>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
 
@@ -16,8 +16,8 @@ const Education = () => {
                     }}
 
                     contentArrowStyle={{ borderRight: '7px solid  rgb(6, 182, 212)' }}
-                    date="2019 - 2020 "
-
+                    date="2018 - 2020 "
+                    dateClassName='text-white md:text-blue-900'
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
 
                 >
@@ -34,10 +34,11 @@ const Education = () => {
                     contentStyle={{ background: "linear-gradient(to right, rgb(99, 102, 241), rgb(236, 72, 153))", color: '#fff' }}
 
                     contentArrowStyle={{ borderRight: '7px solid  rgb(59, 130, 246)' }}
-                    date="2020-2024"
+                    date="2021 - 2024"
+                    dateClassName='text-white md:text-pink-900'
 
                     iconStyle={{ background: 'rgb(236, 72, 153)', color: '#fff' }}
-
+                    iconClassName=''
                 >
                     <div className='space-y-1'>
                         <h3 className="vertical-timeline-element-title text-3xl pb-4">Diploma In Computer Science & Technology</h3>
@@ -56,7 +57,7 @@ const Education = () => {
 
                     contentArrowStyle={{ borderRight: '7px solid  rgb(6, 182, 212)' }}
                     date="2015 - 2017"
-
+                    dateClassName='text-white md:text-green-900'
                     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
 
                 >
