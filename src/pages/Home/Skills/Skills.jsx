ed5994d666b5d42a5dab64c0a9295d598b207ef1
Habibur Rahman <habibur88454@gmail.com>
2024-08-14 11:16:10 +0600

    upadte skills value

--- src/pages/Home/Skills/Skills.jsx
+++ src/pages/Home/Skills/Skills.jsx
@@ -24,7 +24,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-red-900 to-red-600 rounded-lg flex items-center justify-center px-12 py-6'>
                             <div>
                                 <div>
-                                    <img src={htmlImg} alt="" />
+                                    <img className='mx-auto' src={htmlImg} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>Html</p>
                             </div>
@@ -32,7 +32,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-sky-900 to-sky-600 rounded-lg flex items-center justify-center py-6'>
                             <div>
                                 <div>
-                                    <img src={css} alt="" />
+                                    <img className='mx-auto' src={css} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>Css</p>
                             </div>
@@ -40,7 +40,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-yellow-900 to-yellow-600 rounded-lg flex items-center justify-center py-6'>
                             <div>
                                 <div>
-                                    <img src={js} alt="" />
+                                    <img className='mx-auto' src={js} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>Javascript</p>
                             </div>
@@ -48,7 +48,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-indigo-900 to-indigo-600 rounded-lg flex items-center justify-center py-6'>
                             <div>
                                 <div>
-                                    <img src={bootstrap} alt="" />
+                                    <img className='mx-auto' src={bootstrap} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>Bootstrap</p>
                             </div>
@@ -56,7 +56,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-cyan-900 to-cyan-600 rounded-lg flex items-center justify-center py-6'>
                             <div>
                                 <div>
-                                    <img src={tailwind} alt="" />
+                                    <img className='mx-auto' src={tailwind} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>Tailwind</p>
                             </div>
@@ -64,7 +64,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-green-900 to-green-600 rounded-lg flex items-center justify-center py-6'>
                             <div>
                                 <div>
-                                    <img src={node} alt="" />
+                                    <img className='mx-auto' src={node} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>Node</p>
                             </div>
@@ -73,7 +73,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-blue-900 to-blue-600 rounded-lg flex items-center justify-center py-6'>
                             <div>
                                 <div>
-                                    <img src={react} alt="" />
+                                    <img className='mx-auto' src={react} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>React</p>
                             </div>
@@ -81,7 +81,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-pink-900 to-pink-600 rounded-lg flex items-center justify-center py-6'>
                             <div>
                                 <div>
-                                    <img src={express} alt="" />
+                                    <img className='mx-auto' src={express} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>Express</p>
                             </div>
@@ -89,7 +89,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-emerald-900 to-emerald-600 rounded-lg flex items-center justify-center py-6'>
                             <div>
                                 <div>
-                                    <img src={mongoDB} alt="" />
+                                    <img className='mx-auto' className='mx-auto' src={mongoDB} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>MongoDB</p>
                             </div>
@@ -97,7 +97,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-amber-900 to-amber-600 rounded-lg flex items-center justify-center py-6'>
                             <div>
                                 <div>
-                                    <img src={firebase} alt="" />
+                                    <img className='mx-auto' src={firebase} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>Firebase</p>
                             </div>
@@ -105,7 +105,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-zinc-900 to-zinc-600 rounded-lg flex items-center justify-center py-6'>
                             <div>
                                 <div>
-                                    <img src={github} alt="" />
+                                    <img className='mx-auto' src={github} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>Github</p>
                             </div>
@@ -113,7 +113,7 @@ const Skills = () => {
                         <div className='bg-gradient-to-b from-violet-900 to-violet-600 rounded-lg flex items-center justify-center py-6'>
                             <div>
                                 <div>
-                                    <img src={git} alt="" />
+                                    <img className='mx-auto' src={git} alt="" />
                                 </div>
                                 <p className='text-xl mt-2 text-white font-bold'>Git</p>
                             </div>
